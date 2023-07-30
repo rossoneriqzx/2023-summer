185a0fe66ee05f56c518222651b0df93d69bf3de
rossoneriqzx <138179677+rossoneriqzx@users.noreply.github.com>
2023-07-30 15:01:56 +0800

    Add files via upload

---
+++ pj1 2.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+
+import psycopg2
+import pandas as pd
+import mplfinance as mpf
+import datetime
+import matplotlib.pyplot as plt
+import talib
+    
+conn = psycopg2.connect(
+database = "equity_db",
+host = "edw.cy1s98bz8vio.ca-central-1.rds.amazonaws.com",
+port = 5555,
+user = "wealthsigma_etl",
+password = "algofamily")
+cursor = conn.cursor()
+
+def stock_data(stock, start_date, end_date):
+    cursor.execute(f'''
+                SELECT security_symbol, open_price, close_price, high_price, low_price, date_key_int, volume
+                FROM us_equity_daily_finn
+                WHERE security_symbol='{stock}'
+                AND date_key_int BETWEEN {start_date} AND {end_date};
+                ''')
+    df = pd.DataFrame(cursor.fetchall(), columns=['stock', 'Open', 'Close', 'High', 'Low', 'date_key_int', 'Volume'])
+
+    df['date_key_int']=pd.to_datetime(df['date_key_int'],format='%Y%m%d')
+    df = df.set_index("date_key_int")
+    df.index.name = 'Date'
+    return df
+
+def kchart_creator(df):
+    add_plot = [mpf.make_addplot(df['UpperB']),
+                mpf.make_addplot(df['LowerB']),
+                mpf.make_addplot(df['MA20']),
+                mpf.make_addplot(df['vwap'], type='line')]
+    
+    mpf.plot(df,type='candle',title = 'Kchart',
+             ylabel = 'PRICE in USD',
+             style = 'yahoo',
+             addplot = add_plot,
+             volume=True)
+
+
+def Bollinger_band(df):
+     df['MA20'] = talib.MA(df['Close'], timeperiod=20)
+     stddev = talib.STDDEV(df['Close'], timeperiod=20)
+     df['UpperB'] = df['MA20'] + 2 * stddev
+     df['LowerB'] = df['MA20'] - 2 * stddev
+     return df
+
+def vwap(df):
+    v = df['Volume'].values
+    tp = (df['Low'] + df['Close'] + df['High']).div(3).values
+    return df.assign(vwap=(tp * v).cumsum() / v.cumsum())
+
+
+def full_chart(stock, start_date, end_date):
+    df = stock_data(stock, start_date, end_date)
+    df1 = Bollinger_band(df)
+    df2 = vwap(df1)
+
+    kchart_creator(df2)
+    
+
+
+#('AAPL',20221224,20230313)
+
+
+            #add_plot = [mpf.make_addplot(df['UpperB']),
+                        #mpf.make_addplot(df['LowerB']),
+                        #mpf.make_addplot(df['MA20'])]
+
+
+#d是list
+        
+
+#1.
+#画图包装成function
+#输入股票代码，起始日期，终止日期——>图
+#如果股票代码不存在——>“股票不存在”
+#终止日期>起始日期
+#时间跨度bu能超过3个月
+
+#2.
+#价格基础上加一些指标：+volume + vwap+ 布林带
+
+
+
+#3***.
+#把2包装成一个函数。
